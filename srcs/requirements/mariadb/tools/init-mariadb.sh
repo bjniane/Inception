29f771233ea01db84c2b949ccaf24e5d653fa334
toro <bjniane@student.1337.ma>
2025-08-23 16:00:20 -0500

    Done

--- srcs/requirements/mariadb/tools/init-mariadb.sh
+++ srcs/requirements/mariadb/tools/init-mariadb.sh
@@ -1,27 +1,22 @@
 #!/bin/sh
 
-if [ ! -f "/var/lib/mysql/mysql" ]; then
+if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "Initializing MariaDB data directory..."
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 fi
 
-# Start MariaDB server in background
-mysqld --user=mysql --datadir=/var/lib/mysql --bind-address=0.0.0.0 &
-MYSQLD_PID=$!
+service mariadb start
 
-# Wait until MariaDB is ready
-until mysqladmin ping --silent; do
+while ! mysqladmin ping --silent; do
     echo "Waiting for MariaDB to start..."
     sleep 1
 done
 
-# Run setup queries
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
 mysql -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';"
+mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
 
-# Stop background MariaDB
-mysqladmin shutdown
+service mariadb stop
  
-exec mysqld --user=mysql --datadir=/var/lib/mysql --bind-address=0.0.0.0
+exec mysqld --bind-address=0.0.0.0
